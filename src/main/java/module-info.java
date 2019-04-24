c9c4273c60504b93fb1a59d961aedef4f48118ca
Helge Stenström <h.stenstrom@gmail.com>
2019-04-24 23:29:09 +0200

    First successful imports

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module xr3UsageExamples {
+    requires javafx.controls;
+    //requires org.junit.jupiter.api;
+
+    requires jaudiotagger;
+
+    //requires org.jaudiotagger.tag.id3;
+    // requires com.github.goxr3plus;
+    exports org.helgestenstrom;
+
+}
