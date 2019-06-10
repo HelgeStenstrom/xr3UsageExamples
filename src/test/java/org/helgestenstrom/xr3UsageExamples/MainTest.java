e85351a66b8633f6159f35f745d2ada9b7d37171
Helge Stenström <h.stenstrom@gmail.com>
2019-06-10 23:11:25 +0200

    Create MainTest.java

---
+++ src/test/java/org/helgestenstrom/xr3UsageExamples/MainTest.java
@@ -0,0 +1,11 @@
+package org.helgestenstrom.xr3UsageExamples;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void main() {
+        String[] args = {};
+        Main.main(args);
+    }
+}
